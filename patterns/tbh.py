1c5b51991a9cefd0dcc492afcd5417a0c8e0cb6d
Arun <kumar.ak.arun3@gmail.com>
2018-03-19 09:24:52 +0530

    changed tbh algo

--- patterns/tbh.py
+++ patterns/tbh.py
@@ -22,23 +22,20 @@ class TBH(Base):
         candles = self.candles
 
         if hasattr(candles, 'first_candle'):
-            up, lw, height = self.bolinger_bands(candles=candles)
-            rsi14 = self.rsi(candles=candles)
+            up, lw, height = self.bolinger_bands(candles=candles, period=13, num_of_std=2.2)
+            rsi14 = self.rsi(candles=candles, period=13)
             K, D = self.stoc_occilator(candles=candles)
             aroon_up, aroon_down = self.aroon(candles=candles, period=14)
 
             # loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
             # predicted_price = loaded_model.predict([[up[26] - candles.first_candle.candle_close, lw[26] - candles.first_candle.candle_close, candles.first_candle.candle_height - candles.second_candle.candle_height, rsi14[28], K[28], D[28], aroon_up[29], aroon_down[29]]])
-            logger.info("Height:'%i'", height[26])
-            logger.info("First candle close:'%f'", candles.first_candle.candle_close)
-            logger.info("AR Up: '%i', AR Down:'%i'", aroon_up[29], aroon_down[29])
+            logger.info("SecondCandleClose:'%i', BBUp:'%i', BBLow:'%i', RSI: '%i', ARUp:'%i', ARDown:'%i'", candles.second_candle.candle_close, up[28], lw[28], rsi14[28], aroon_up[29], aroon_down[29])
 
-            if candles.first_candle.candle_close <= lw[26] and height[26] > 499:
-                if candles.first_candle.candle_type == "red" and candles.second_candle.candle_type == "green":
-                    if candles.second_candle.candle_height >= (candles.first_candle.candle_height / 2):
-                        if aroon_down[29] > 60:
-                            logger.info("Lower Band:'%f', First candle close: '%f'.", lw[26], candles.first_candle.candle_close)
-                            return True
+            if candles.second_candle.candle_close < lw[28] and height[28] > 499:
+                # if candles.first_candle.candle_type == "red" and candles.second_candle.candle_type == "green":
+                # if candles.second_candle.candle_height >= (candles.first_candle.candle_height / 2):
+                if rsi14[28] < 25:
+                    return True
 
     def put(self):
         """Method to check put pattern."""
@@ -46,17 +43,16 @@ class TBH(Base):
         candles = self.candles
 
         if hasattr(candles, 'first_candle'):
-            up, lw, height = self.bolinger_bands(candles=candles)
-            rsi14 = self.rsi(candles=candles)
+            up, lw, height = self.bolinger_bands(candles=candles, period=13, num_of_std=2.2)
+            rsi14 = self.rsi(candles=candles, period=13)
             K, D = self.stoc_occilator(candles=candles)
             aroon_up, aroon_down = self.aroon(candles=candles, period=14)
 
             # loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
             # predicted_price = loaded_model.predict([[up[26] - candles.first_candle.candle_close, lw[26] - candles.first_candle.candle_close, candles.first_candle.candle_height - candles.second_candle.candle_height, rsi14[28], K[28], D[28], aroon_up[29], aroon_down[29]]])
             
-            if candles.first_candle.candle_close >= up[26] and height[26] > 499:
-                if candles.first_candle.candle_type == "green" and candles.second_candle.candle_type == "red":
-                    if candles.second_candle.candle_height >= (candles.first_candle.candle_height / 2):
-                        if aroon_up[29] > 60:
-                            logger.info("High Band:'%f', First candle close: '%f'.", up[26], candles.first_candle.candle_close)
-                            return True
+            if candles.first_candle.candle_close >= up[28] and height[28] > 499:
+                # if candles.first_candle.candle_type == "green" and candles.second_candle.candle_type == "red":
+                # if candles.second_candle.candle_height >= (candles.first_candle.candle_height / 2):
+                if rsi14[28] > 75:
+                    return True
